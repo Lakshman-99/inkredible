4c82570887bf5b9e0b1f074202a54a90c053c20e
Lakshman S <slakshman664@gmail.com>
2022-12-12 22:08:34 +0530

    v3

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ from werkzeug.utils import secure_filename
 from logic import find_text
 from docx import Document
 from fpdf import FPDF
+from pathlib import Path
+from pdf2image import convert_from_path as CFP
 import os
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
-app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER2'] = 'static/saves/'
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'pdf'])
 
+#remove files in uploads & pdf_img
+[f.unlink() for f in Path("static/uploads/").glob("*") if f.is_file()]
+[f.unlink() for f in Path("static/saves/").glob("*") if f.is_file()]
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
@@ -30,6 +36,10 @@ def login():
 def imagetotext():
 	return render_template('imgtotxt.html')
 
+@app.route('/pdftotext')
+def pdftotext():
+	return render_template('pdftotxt.html')
+
 @app.route('/upload', methods=['POST'])
 def upload():
 	uploaded_files = request.files.getlist("file[]")
@@ -44,7 +54,32 @@ def upload():
 			text = find_text(filepath)
 			file_details.append([filename, text])
 
-	return render_template('display.html', files=file_details)
+	return render_template('imgtotxt_disp.html', files=file_details)
+
+@app.route('/upload2', methods=['POST'])
+def upload2():
+	uploaded_files = request.files.getlist("file[]")
+	if not any(f for f in uploaded_files):
+		return redirect(url_for('index'))
+	file_details = []
+	for file in uploaded_files:
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			filepath = os.path.join(app.config['UPLOAD_FOLDER2'], filename)
+			file.save(filepath)
+			img = CFP(filepath, poppler_path = os.getcwd() + r'\poppler-0.68.0\bin')
+			for i in range(len(img)):
+				fname = 'page'+ str(i) +'.jpg'
+				fpath = os.path.join(app.config['UPLOAD_FOLDER'], fname)
+				img[i].save(fpath, 'JPEG')
+				text = find_text(fpath)
+				file_details.append([fname, text])
+
+	return render_template('imgtotxt_disp.html', files=file_details)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/download', methods=['POST'])
 def save_file():
@@ -55,8 +90,8 @@ def save_file():
 		document = Document()
 		for text in texts:
 			document.add_paragraph(text.replace("\n", ""))
-		document.save('extracted_text.docx')
-		return send_file("extracted_text.docx",as_attachment=True)
+		document.save(os.path.join(app.config['UPLOAD_FOLDER2'], 'extracted_text.docx'))
+		return send_file(os.path.join(app.config['UPLOAD_FOLDER2'], 'extracted_text.docx'),as_attachment=True)
 
 	elif option == "pdf":
 		texts = "\n".join(texts)
@@ -66,14 +101,14 @@ def save_file():
 		pdf.set_font('Times', '', 10)
 		for text in texts.split("\n"):
 			pdf.cell(0, 8, text.encode('utf-8').decode('latin-1'), 0, 1)
-		pdf.output('extracted_text.pdf', 'F')
-		return send_file("extracted_text.pdf",as_attachment=True)
+		pdf.output(os.path.join(app.config['UPLOAD_FOLDER2'], 'extracted_text.pdf'), 'F')
+		return send_file(os.path.join(app.config['UPLOAD_FOLDER2'], 'extracted_text.pdf'),as_attachment=True)
 
 	elif option == "txt":
-		with open("extracted_text.txt", "w") as f:
+		with open(os.path.join(app.config['UPLOAD_FOLDER2'], "extracted_text.txt"), "w") as f:
 			for text in texts:
 				f.write(text.replace("\n", ""))
-		return send_file("extracted_text.txt",as_attachment=True)
+		return send_file(os.path.join(app.config['UPLOAD_FOLDER2'], "extracted_text.txt"),as_attachment=True)
 
 	return ""
 
